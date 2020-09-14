3bc953f2407bb3922e2e19dc785d4cba4655b813
codertyler <codertylerasai@gmail.com>
2020-09-14 21:44:45 +0000

    initial push

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log('❤' + 'Assertion Passed: ' + actual + " === " + expected);
+  } else {
+    console.log('🚫' + 'Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+
+};
+
+//Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
