7b90d560cdcb4c88f18304ef846ff9aecbb0dec1
codertylerasai@gmail.com <codertylerasai@gmail.com>
2020-09-14 16:48:19 -0600

    "fixed esint"

--- tail.js
+++ tail.js
@@ -1,21 +1,21 @@
 const assertEqual = function(actual, expected) {
 
-    if (actual === expected) {
-      console.log(`❤   Assertion Passed: ${actual}  ===  ${expected}`);
-    } else {
-      console.log(`🚫  Assertion Failed: ${actual} !== ${expected}`);
-    }
-  
-  };
-
-  const tail = function(arr) {
-
-    let newArr = arr.slice(1);
-    return newArr;
-
+  if (actual === expected) {
+    console.log(`❤   Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🚫  Assertion Failed: ${actual} !== ${expected}`);
   }
+  
+};
 
-  // Test Case: Check the original array 
+const tail = function(arr) {
+
+  let newArr = arr.slice(1);
+  return newArr;
+
+};
+
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
