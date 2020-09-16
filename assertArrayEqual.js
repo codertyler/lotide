3af984a953425aac6b19080fe5f26a8f14e33ab5
codertyler <codertylerasai@gmail.com>
2020-09-16 02:27:31 +0000

    first commit

---
+++ assertArrayEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = function(arr1, arr2) {
+    
+  
+	if (arr1.length !== arr2.length) console.log("Arrays are not an equal length!");
+
+	for (var i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) console.log("Items in array are not the same!");
+	}
+
+	return console.log("Arrays are equal!");
+
+  
+
+
+}
+
+eqArrays([1, 2, 3], [1,2,3]);
