3489bde21d3e63e9cbc47530bc3e0ab0e70d6333
codertyler <codertylerasai@gmail.com>
2020-09-16 02:21:38 +0000

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`❤   Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🚫  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+    
+  
+	if (arr1.length !== arr2.length) return false;
+
+	for (var i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) return false;
+	}
+
+	return true;
+
+  
+
+
+}
+
+assertEqual(eqArrays([1, 2, 3], [1,2,3]), true);
