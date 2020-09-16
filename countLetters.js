9cbba5b2ce14a39033f539b37b36fa4bcd3197f9
codertyler <codertylerasai@gmail.com>
2020-09-16 21:10:48 +0000

    initial commit

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`❤   Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🚫  Asserretion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(str) {
+  results = {};
+  let newStr = str.split(" ");
+  let useStr = newStr.join("");
+
+  console.log(useStr);
+
+  for (const letter of useStr) {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+  }
+
+  return results;
+
+}
+
+console.log(countLetters("lighthouse in the house"));
