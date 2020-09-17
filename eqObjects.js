7b17645bb67f0d8b50d77b5115be574bbf9de3af
codertyler <codertylerasai@gmail.com>
2020-09-17 04:20:25 +0000

    initial commit

--- eqObjects.js
+++ eqObjects.js
@@ -10,20 +10,23 @@ const assertEqual = function(actual, expected) {
 
 const eqObjects = function(object1, object2) {
 
-
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  }
+  };
+
 
   for (let item of Object.keys(object1)) {
     if (object1[item] !== object2[item]) {
       return false;
-    } else if (Array.isArray(object1[item], object2[item])) {
-      eqArrays(object1[item], object2[item]);
+      } else if (Array.isArray(object1[item])) {
+        eqArrays(object1[item], object2[item]);
     } else {
       return true;
     }
   }
+
+  
+  
 
 
   
@@ -43,6 +46,7 @@ const eqArrays = function(arr1, arr2) {
 }
 
 
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 
@@ -54,8 +58,8 @@ assertEqual(eqObjects(ab, abc), false);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(dc.d);
 assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
