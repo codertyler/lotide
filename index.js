b824ed9c78a5eb2577e3cc05d8bb3251b7154955
codertyler <codertylerasai@gmail.com>
2020-09-22 02:34:43 +0000

    made to object

---
+++ index.js
@@ -0,0 +1,33 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findkey');
+const takeUntil = rqequire('./takeUntil');
+
+
+
+
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  takeUntil: takeUntil
+
+
+
+
+
+};
--- tail.js
+++ tail.js
@@ -5,7 +5,6 @@ const tail = function(arr) {
 
 };
 
-let x = [3,4];
-console.log(tail([1,3,4]));
+
 
 module.exports = tail;
