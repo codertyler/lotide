19e2832512db0a36911de4a7459d686cc18c424e
codertyler <codertylerasai@gmail.com>
2020-09-22 02:54:31 +0000

    npm ready

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert;
 
 describe ("#middle", () => {
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deeEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 
   it("returns [2] for [1, 2, 3]", () => {
